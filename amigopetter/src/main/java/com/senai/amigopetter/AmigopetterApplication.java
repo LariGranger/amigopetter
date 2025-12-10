57a540b24b5cdb33f1d7acff841c4dc639c6f7ea
Larissa Pinheiro <116392603+LariGranger@users.noreply.github.com>
2025-12-10 00:31:13 +0000

    initial commit

---
+++ amigopetter/src/main/java/com/senai/amigopetter/AmigopetterApplication.java
@@ -0,0 +1,13 @@
+package com.senai.amigopetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigopetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigopetterApplication.class, args);
+	}
+
+}
